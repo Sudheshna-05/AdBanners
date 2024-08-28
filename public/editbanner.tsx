7c609d2e5196500b863464e4c156b6352329995f
Sudheshna-05 <tsudheshnareddy@gmail.com>
2024-08-28 15:46:02 +0530

    first commit

--- public/editbanner.tsx
+++ public/editbanner.tsx
@@ -45,7 +45,7 @@ const EditBannerTemplateBs: React.FC<EditBannerTemplateBsProps> = ({ open, banne
   };
 
   return (
-    <Dialog open={open} onClose={onclose}>
+    <Dialog open={open} onClose={onClose}>
       <DialogTitle>Edit Banner</DialogTitle>
       <DialogContent>
           <TextField
@@ -98,7 +98,7 @@ const EditBannerTemplateBs: React.FC<EditBannerTemplateBsProps> = ({ open, banne
           />
           </DialogContent>
           <DialogActions>
-          <Button onClick={onclose} color="primary">Cancle</Button>
+          <Button onClick={onClose} color="primary">Cancle</Button>
           <Button onClick={handleSave} variant="contained" color="primary">
             Save
           </Button>
